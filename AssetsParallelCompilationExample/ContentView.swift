c09e14cde914108afeaa3dac4c57ea754c238d9d
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2026-02-19 07:42:35 +0300

    add code for access

--- AssetsParallelCompilationExample/ContentView.swift
+++ AssetsParallelCompilationExample/ContentView.swift
@@ -6,13 +6,12 @@
 //
 
 import SwiftUI
+import UIResources
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            ChromaticIcons.SwiftUI.sms.foregroundStyle(.green)
             Text("Hello, world!")
         }
         .padding()
--- UIResources/UIResources.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIResources.swift
-//  UIResources
-//
-//  Created by tmp on 19/02/2026.
-//
-
-import Foundation
-
