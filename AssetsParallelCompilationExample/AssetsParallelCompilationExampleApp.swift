dfe46b3fb72aa74e1bc72c09e4d1da757c03d43a
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2026-02-19 06:05:42 +0300

    Initial Commit

---
+++ AssetsParallelCompilationExample/AssetsParallelCompilationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsParallelCompilationExampleApp.swift
+//  AssetsParallelCompilationExample
+//
+//  Created by tmp on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AssetsParallelCompilationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
